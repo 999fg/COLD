e84c359f78b053dfcaa645f5ce9882df94fd0366
Jaemin Shin <999fg@kaist.ac.kr>
2017-12-02 15:32:08 +0900

    request.py for testing api.py

--- flaskrest/request.py
+++ flaskrest/request.py
@@ -10,15 +10,15 @@ api = Api(app)
 if __name__=='__main__':
 	app.run(host='0.0.0.0')
 '''
-url = 'http://172.17.0.3:5000/' #IP address of other machine
+url = 'http://192.168.0.7:5000/' #IP address of other machine
 while(True):
 	print ('Client -> Control Plane, /upload (POST) : 1')
 	rest_choice = input()
 	if int(rest_choice) == 1:
 		url = url + 'upload'
-		file_info = '{"file_name", "file_block_name": ["file_piece1", "file_piece2"], "file_size": 10000000, "block_size": [500000, 500000]}'
+		file_info = '{"file_name": "file.txt", "file_block_name": ["file_piece1", "file_piece2"], "file_size": 10000000, "block_size": [500000, 500000]}'
 		headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-		response = requests.post(url, data=file_info, headers = headers)
+		response = requests.post(url, data=json.dumps(file_info), headers = headers)
 		json_dict = response.json()
 		print (json_dict.get('message'))
 	
